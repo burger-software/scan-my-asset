1625e91451a49657ac9fb5b72c730437d692774e
Tom Burger <tburger@glaass.net>
2024-02-23 21:33:28 +0100

    support for the assets from url

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import "./style.scss"
 import { RenderPage } from "./page";
 import { RenderNavigation } from "./navigation";
 import { BindWelcome } from "./welcome";
+import { BindResult } from "./result";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = RenderNavigation() + RenderPage();
 
-BindWelcome();
+const params = new URLSearchParams(window.location.search);
+const asset = params.get('asset');
+asset ? BindResult(asset) : BindWelcome();
